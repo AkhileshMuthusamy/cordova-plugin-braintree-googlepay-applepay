c0a26abf56c7d94fcb8321ca681f44030cbfeebf
Akhilesh Muthuamy <akhilesh.muthusamy@rideco.com>
2022-11-02 22:10:45 -0400

    Add sample swift code

--- src/ios/BraintreeGooglePayApplePay.swift
+++ src/ios/BraintreeGooglePayApplePay.swift
@@ -1,28 +1,13 @@
 /********* BraintreeGooglePayApplePay.swift Cordova Plugin Implementation *******/
 
-#import <Cordova/CDV.h>
-
-@interface BraintreeGooglePayApplePay : CDVPlugin {
-  // Member variables go here.
+@objc(BraintreeGooglePayApplePay) class BraintreeGooglePayApplePay : CDVPlugin {
+  @objc(coolMethod:) 
+  func coolMethod(command: CDVInvokedUrlCommand) {
+    // Set the plugin result to fail.
+    var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "The Plugin Failed");
+    // Set the plugin result to succeed.
+    pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "The plugin succeeded");
+    // Send the function result back to Cordova.
+    self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+  }
 }
-
-- (void)coolMethod:(CDVInvokedUrlCommand*)command;
-@end
-
-@implementation BraintreeGooglePayApplePay
-
-- (void)coolMethod:(CDVInvokedUrlCommand*)command
-{
-    CDVPluginResult* pluginResult = nil;
-    NSString* echo = [command.arguments objectAtIndex:0];
-
-    if (echo != nil && [echo length] > 0) {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    } else {
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
-    }
-
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-}
-
-@end
